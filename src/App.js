186b962ccc03473e9f3f8cc86ebc2b8261431a6d
Hayato KAJIYAMA <kajiyamah61@COAMAC17000052.local>
2019-05-10 15:59:42 +0900

    add: imagepicker

--- src/App.js
+++ src/App.js
@@ -7,23 +7,72 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform,
+        StyleSheet,
+        Text,
+        View,
+        Button,
+        ScrollView,
+        Image} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const options = {
+  title: 'Select Avatar',
+  customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+  storageOptions: {
+    skipBackup: true,
+    path: 'images',
+  },
+};
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+
+
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      photos: false,
+    }
+  }
+
+  _handleButtonPress = () => {
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        const source = { uri: response.uri };
+
+        // You can also display the image using data:
+        // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+        this.setState({
+          photos: source,
+        });
+      }
+    });
+  }
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Button
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Button style={styles.button} title="Load Images" onPress={this._handleButtonPress} />
+        <ScrollView>
+          {this.state.photos ?
+           <Image source={ this.state.photos } style={{ width: 200, height: 200 }} />
+           : <Text> Please select Picture</Text>}
+        </ScrollView>
       </View>
     );
   }
@@ -36,10 +85,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  button: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    color: "#841584",
+    marginTop: 40,
   },
   instructions: {
     textAlign: 'center',
